ea7260ff9785cfc08031ab841399260411abae19
Matheus Ferreira <ferreira@MacBook-Air-de-Matheus.local>
2024-03-20 23:14:03 -0300

    Altera o retorno dos metodos/ Implemnta propriedade de Arestas e Vertices/ Implementa Teorema de Ore

--- AV1/Classe.py
+++ AV1/Classe.py
@@ -1,46 +1,71 @@
 class Grafo:
     def __init__(self):
         self.grafo = {}
-    
+
     @property        
     def quantidadeVertices(self):
         return len(self.grafo)
+    
+    @property
+    def vertices(self):
+        return list(self.grafo.keys())
+    
+    @property
+    def arestas(self):
+        lista_arestas = set()
+        for vertice, vizinhos in self.grafo.items():
+            for vizinho in vizinhos:
+                if (vizinho, vertice) not in lista_arestas:
+                    lista_arestas.add((vertice, vizinho))
+        return list(lista_arestas)
 
     def adicionaVertice(self, vertice):
         if vertice not in self.grafo:
             self.grafo[vertice] = []
 
     def adicionaAresta(self, vertice1, vertice2):
-        if vertice1 in self.grafo:
+        if vertice1 not in self.grafo:
+            self.grafo[vertice1] = []
+        if vertice2 not in self.grafo:
+            self.grafo[vertice2] = []
+        if vertice2 not in self.grafo[vertice1]:
             self.grafo[vertice1].append(vertice2)
-        else:
-            self.grafo[vertice1] = [vertice2]
-
-        if vertice2 in self.grafo:
+        if vertice1 not in self.grafo[vertice2]:
             self.grafo[vertice2].append(vertice1)
-        else:
-            self.grafo[vertice2] = [vertice1]
     
     def mostraGrafo(self):
         for vertice in self.grafo:
-            print(f"{vertice} -> {', '.join([str(vizinho) for vizinho in self.grafo[vertice]])}")
+            print(f"{vertice} -> {', '.join(map(str, self.grafo[vertice]))}")
     
-    def grauVertice(self,vertice):
-        if vertice in self.grafo:
-            return len(self.grafo[vertice])
+    def grauVertice(self, vertice):
+        return len(self.grafo.get(vertice, []))
             
     def quantidadeArestas(self):
-        total_graus = sum(self.grauVertice(vertice) for vertice in self.grafo)
-        return total_graus / 2
+        return sum(self.grauVertice(vertice) for vertice in self.grafo) / 2
     
     def dirac(self):
         if self.quantidadeVertices < 3:
-            print("Grafo não Hamiltoniano pelo Teorema de Dirac")
-            return
+            return "Grafo não Hamiltoniano pelo Teorema de Dirac"
         
         for vertice in self.grafo:
             if self.grauVertice(vertice) < self.quantidadeVertices / 2:
-                print("Grafo não Hamiltoniano pelo Teorema de Dirac")
-                return
+                return "Grafo não Hamiltoniano pelo Teorema de Dirac"
 
-        print("Grafo Hamiltoniano pelo Teorema de Dirac")
+        return "Grafo Hamiltoniano pelo Teorema de Dirac"
+        
+    def ore(self):
+        if self.quantidadeVertices < 3:
+            return "Grafo não Hamiltoniano pelo Teorema de Ore"
+        
+        for v1 in self.grafo:
+            for v2 in self.grafo:
+                if v1 != v2 and v2 not in self.grafo[v1]:
+                    if self.grauVertice(v1) + self.grauVertice(v2) < self.quantidadeVertices:
+                        return "Grafo não Hamiltoniano pelo Teorema de Ore"
+                    
+        return "Grafo Hamiltoniano pelo Teorema de Ore"
+    
+    def bondyChvatal(self):
+        pass
+       
+       
